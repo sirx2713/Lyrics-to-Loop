8a54572b3a5aac02377331050e9c2667a7278bea
Mabende <belindamabende@gmail.com>
2024-05-31 04:30:50 +0200

    While looping sing along!

---
+++ lyrics.js
@@ -0,0 +1,5 @@
+let lyricsCount = 100;
+while (lyricsCount < 100 && lyricsCount !== 0){
+    console.log(lyricsCount + "bottles of beer on the wall, " + lyricsCount + "bottles of beer.\n" + "Take one down and pass it around, " + (lyricsCount - 1) + " bottles of beer on the wall.");
+    lyricsCount--;
+    }
